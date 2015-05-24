fe3aecb2037ac71a7cbe16a42f492b0d7d9dca72
Sonia U <sonia9uppal@gmail.com>
2015-05-24 15:05:32 -0700

    Added a style.css and changed the layout of the website

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	port = process.env.PORT || 5000,
 	app = express();
 
+app.get('public/style.css');
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
 
